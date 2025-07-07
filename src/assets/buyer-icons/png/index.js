6972060eb4d9e6a64196bccc5c1081f5bafddb57
ErmanDev <ermanfaminiano020@gmail.com>
2025-07-07 09:20:44 +0800

    Add new buyer icons and images, implement InfoCard and PromoBadge components, and update Shop screen layout

---
+++ src/assets/buyer-icons/png/index.js
@@ -0,0 +1,26 @@
+import banner1 from './image-1.png';
+import banner2 from './image-2.png';
+import banner3 from './image-3.png';
+
+import genus1 from './alocasia.png';
+import genus2 from './anthurium.png';
+import genus3 from './begonia.png';
+import genus4 from './hoya.png';
+import genus5 from './monstera.png';
+import genus6 from './scindapsus.png';
+import genus7 from './syngonium.png';
+import genus8 from './philodendron.png';
+
+export {
+  banner1,
+  banner2,
+  banner3,
+  genus1,
+  genus2,
+  genus3,
+  genus4,
+  genus5,
+  genus6,
+  genus7,
+  genus8,
+};
