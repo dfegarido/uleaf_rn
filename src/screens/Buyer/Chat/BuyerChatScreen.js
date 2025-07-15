e4ac874a41e3c700687140b855c41f630cc03fd6
darwin <darwinfegarido@gmail.com>
2025-07-15 14:20:39 +0800

    Pull Chat Navigation

---
+++ src/screens/Buyer/Chat/BuyerChatScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MessagesScreen from '../../MessagesScreen/MessagesScreen';
+
+const BuyerChatScreen = ({navigation}) => {
+  return <MessagesScreen navigation={navigation} />;
+};
+
+export default BuyerChatScreen;
