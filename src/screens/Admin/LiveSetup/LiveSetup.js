b92b4b415de8452faa124b4b696152700be4a0f1
ErmanDev <ermanfaminiano020@gmail.com>
2025-08-11 05:16:02 +0800

    added admin UI

---
+++ src/screens/Admin/LiveSetup/LiveSetup.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const LiveSetup = () => {
+    return (
+        <View>
+            <Text>Live Setup</Text>
+        </View>
+    );
+};
+
+export default LiveSetup;
---
+++ src/screens/Admin/Taxonomy/Taxonomy.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Taxonomy = () => {
+    return (
+        <View>
+            <Text>Taxonomy</Text>
+        </View>
+    );
+};
+
+export default Taxonomy;
