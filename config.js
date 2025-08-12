6e096c1fd5b96a13a65cdcdbfbda41eb0f4c8eaf
darwin <darwinfegarido@gmail.com>
2025-08-12 11:11:47 +0800

    Venmo payment

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Replace with your actual live PayPal/Venmo URL https://i-leaf-u.web.app/
-// for testing purposes, you can use the staging URL https://uleaf-36de7.web.app
+// for testing purposes, you can use the staging URL https://uleaf-36de7.web.app/
 export const paymentPaypalVenmoUrl = 'https://i-leaf-u.web.app/'; 
